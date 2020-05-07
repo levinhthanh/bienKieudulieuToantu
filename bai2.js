9152713452f7c2516c5a576c22d8b53e24271608
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-05-07 13:19:27 +0700

    them

---
+++ bai2.js
@@ -0,0 +1,4 @@
+let height=100;
+let width=50;
+let area = height * width;
+document.write("Diện tích hình chữ nhật = " +area);
---
+++ bai3.js
@@ -0,0 +1,9 @@
+
+let a = parseInt(prompt('Nhập số thứ nhất: a='));
+let b = parseInt(prompt('Nhập số thứ nhất: b='));
+let c = a%b;
+if (c==0) {
+        document.write("a là bội số của b");
+} else {
+        document.write("a không phải là bội số của b");
+}
